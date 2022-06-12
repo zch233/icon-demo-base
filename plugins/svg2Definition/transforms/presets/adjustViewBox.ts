cf42295c51f7c52a067db058666a44825f47ea80
zhangchao <529743595@qq.com>
2022-06-12 23:10:27 +0800

    feat: add generateIcons-['filled','outlined','twotone','color']

---
+++ plugins/svg2Definition/transforms/presets/adjustViewBox.ts
@@ -0,0 +1,35 @@
+import { assignAttrsAtTag } from '../index';
+import { TransformFactory } from '../../index';
+
+// version < antd@3.9
+const OLD_ICON_NAMES = [
+  'step-backward',
+  'step-forward',
+  'fast-backward',
+  'fast-forward',
+  'forward',
+  'backward',
+  'caret-up',
+  'caret-down',
+  'caret-left',
+  'caret-right',
+  'retweet',
+  'swap-left',
+  'swap-right',
+  'loading',
+  'loading-3-quarters',
+  'coffee',
+  'bars',
+  'file-jpg',
+  'inbox',
+  'shopping-cart',
+  'safety',
+  'medium-workmark'
+];
+
+export const adjustViewBox: TransformFactory = assignAttrsAtTag(
+  'svg',
+  ({ name }) => ({
+    viewBox: OLD_ICON_NAMES.includes(name) ? '0 0 1024 1024' : '64 64 896 896'
+  })
+);
---
+++ plugins/svgo/index.ts
@@ -0,0 +1,14 @@
+import SVGO from 'svgo';
+import { createTrasformStreamAsync } from '../creator';
+
+export const svgo = (options: SVGO.OptimizeOptions) => {
+  // const optimizer = new SVGO(options);
+  return createTrasformStreamAsync(async (before) => {
+    const result = await SVGO.optimize(before, options);
+    if ("data" in result) {
+      return result.data
+    } else {
+      throw 'svgo optimize error'
+    }
+  });
+};
