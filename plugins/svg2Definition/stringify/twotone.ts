d677c84147ef3818d0ff76fe26ab3cb79574f85d
zhangchao <529743595@qq.com>
2022-06-13 16:53:28 +0800

    fix: fix twotoneStringify

--- plugins/svg2Definition/stringify/twotone.ts
+++ plugins/svg2Definition/stringify/twotone.ts
@@ -1,45 +1,27 @@
-import { StringifyFn, AbstractNodeDefinition } from '..';
-import { applyTo, map, join, pipe, ap, toUpper, toLower, evolve, toPairs, replace, apply, flip, compose, zipWith, call, assoc, clone, prop } from 'ramda';
+import { StringifyFn, AbstractNodeDefinition } from '../index';
+import cloneDeep from 'lodash.clonedeep'
 
 const __PLACEHOLDER__ = 'TWOTONE_ICON_FUNCTION_HOLDER';
 const __PRIMARY_COLOR__ = 'primaryColor';
 const __SECONDARY_COLOR__ = 'secondaryColor';
 
-const getRegExpFromColors = pipe(
-    ap([toUpper, toLower]),
-    map(color => `("${color}")`),
-    join('|'),
-    content => new RegExp(content, 'g')
-);
+const getRegExpFromColors = (colors: string[]) => new RegExp([...colors.map(v => v.toUpperCase()), ...colors.map(v => v.toLowerCase())].map(color => `("${color}")`).join('|'), 'g')
 
-const colorsReplacer = applyTo({
-    [__PRIMARY_COLOR__]: ['#333', '#333333'],
-    [__SECONDARY_COLOR__]: ['#E6E6E6', '#D9D9D9', '#D8D8D8'],
-})(
-    pipe(
-        evolve({
-            [__PRIMARY_COLOR__]: getRegExpFromColors,
-            [__SECONDARY_COLOR__]: getRegExpFromColors,
-        }),
-        toPairs,
-        map(compose<ReplaceType, FlippedReplaceType, ApplyFlippedReplaceType>(apply, flip)(replace)),
-        apply<(str: string) => string, (str: string) => string>(pipe)
-    )
-);
+const colorsReplacer = (icon: string) => {
+  const colorsPairs: [string, RegExp][] = [[__PRIMARY_COLOR__, getRegExpFromColors(['#333', '#333333'])],[__SECONDARY_COLOR__, getRegExpFromColors(['#E6E6E6', '#D9D9D9', '#D8D8D8'])]]
+  const fns = colorsPairs.map((v) => (str: string) => str.replace(v[1], v[0]))
+  return fns.reduce((a,b) => b(a),icon)
+};
 
-type ReplaceType = (pattern: RegExp, replacement: string) => (str: string) => string;
+const duplicate = <T>(n: T): [T, T] => [cloneDeep(n), cloneDeep(n)];
 
-type FlippedReplaceType = (replacement: string, pattern: RegExp) => (str: string) => string;
+const createFunctionStr = (content:string) => `function render(${__PRIMARY_COLOR__}, ${__SECONDARY_COLOR__}) { return ${content}; }`;
 
-type ApplyFlippedReplaceType = ([replacement, pattern]: [string, RegExp]) => (str: string) => string;
-
-const duplicate = <T>(n: T): [T, T] => [clone(n), clone(n)];
-
-export const twotoneStringify: StringifyFn = pipe(
-    duplicate,
-    zipWith<(asnd: AbstractNodeDefinition) => string, AbstractNodeDefinition, string>(call as any, [
-        pipe(assoc('icon', __PLACEHOLDER__), JSON.stringify),
-        pipe(prop('icon'), JSON.stringify, colorsReplacer, content => `function render(${__PRIMARY_COLOR__}, ${__SECONDARY_COLOR__}) { return ${content}; }`),
-    ]),
-    ([hasPlaceholderContent, iconFunctionContent]) => replace(`"${__PLACEHOLDER__}"`, iconFunctionContent, hasPlaceholderContent)
-);
+export const twotoneStringify: StringifyFn = (iconData) => {
+  const iconDataRepeat = duplicate(iconData);
+  const [hasPlaceholderContent, iconFunctionContent] = [
+    (data: AbstractNodeDefinition) => JSON.stringify({...data, icon: __PLACEHOLDER__}),
+    (data: AbstractNodeDefinition) => createFunctionStr(colorsReplacer(JSON.stringify(data.icon))),
+  ].map((fn,i) => fn(iconDataRepeat[i]))
+  return hasPlaceholderContent.replace(`"${__PLACEHOLDER__}"`, iconFunctionContent)
+};
